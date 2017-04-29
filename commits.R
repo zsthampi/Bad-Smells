1a1b7deb7e80b5cd30fb544f1b227c4795d85df5
zsthampi <zsthampi@ncsu.edu>
2017-04-29 17:25:03 -0400

    Lots of stuff

--- commits.R
+++ commits.R
@@ -7,8 +7,8 @@ commits$created_by <- as.character(commits$created_by)
 commits_per_team <-  sort(table(commits$team))
 
 # Barplot of number of commits per team
-b <- barplot(commits_per_team,ylim=c(0,300),main="Total number of commits per team")
-text(b,unname(commits_per_team),pos = 3)
+b <- barplot(commits_per_team,ylim=c(0,400),main="Total number of commits per team")
+# text(b,unname(commits_per_team),pos = 3)
 
 # Plot pie chart of percentage of commits per user within a team
 # teams <- unique(commits$team)
@@ -30,5 +30,64 @@ for (i in 1:ncol(x)) {
     x[,i] <- x[,i]/sum(x[,i])
 }
 
-barplot(x,legend.text = TRUE)
+barplot(x,legend.text = TRUE,args.legend = list(x="top",inset=c(0,-0.2),horiz=TRUE))
 
+
+# Get numbers of commits per week
+startTime <- strptime("2017-01-10 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
+endTime <- strptime("2017-05-05 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
+timeList <- c(startTime)
+while(startTime<endTime) {
+    startTime <- startTime + (7*24*60*60)
+    timeList <- c(timeList,startTime)
+}
+
+commits$week <- NA
+for (j in 1:nrow(commits)) {
+commits$week[j] <- (function(x,y) {
+    n <- length(y)
+    return <- NA
+    for (i in 1:n-1) {
+        if (x>=y[i] && x<y[i+1]) {
+            return <- i
+        }
+    }
+    return
+}) (commits$created_at[j],timeList) }
+
+commits$week <- as.factor(commits$week)
+commits$created_by <- as.factor(commits$created_by)
+
+# Plot graph of commits based on week
+# for (team in unique(commits$team)) {
+# 	team_data <- commits[commits$team==team,]
+# 	plot(table(team_data[team_data$created_by=='user1',]$week),xlim=c(0,18),type="p",col="red")
+# 	lines(table(team_data[team_data$created_by=='user2',]$week),xlim=c(0,18),type="p",col="green")
+# 	lines(table(team_data[team_data$created_by=='user3',]$week),xlim=c(0,18),type="p",col="brown")
+# 	if (is.element('user4',unique(team_data$created_by))==TRUE) {
+# 		lines(table(team_data[team_data$created_by=='user4',]$week),xlim=c(0,18),type="p",col="black")
+# 	}
+# }
+
+colRamp <- colorRampPalette(c("white", "black"))
+plot(rep(1,length(timeList)),pch = 15,cex = 5,ylim=c(0,5),col=colRamp(length(timeList)))
+points(rep(2,length(timeList)),pch = 15,cex = 5,col=colRamp(length(timeList)))
+
+colRamp <- colorRampPalette(c("white", "black"))
+plot(rep(1,length(timeList)),pch = 15,cex = 5,ylim=c(0.5,1.5),ylab="",xlab="Scale",col=colRamp(length(timeList)),yaxt='n',xaxt='n')
+
+par(las=1)
+colRamp <- colorRampPalette(c("white", "black"),interpolate = c("linear"))
+for (team in unique(commits$team)) {
+	team_data <- commits[commits$team==team,]
+	x <- table(team_data$created_by,team_data$week)
+	plot(rep(1,length(timeList)),pch = 15,cex = 5,ylim=c(0,5),col=colRamp(max(x)+1)[x[1,]+1],yaxt='n',main=paste("Color scale of number of commits per week : ",team),xlab="Week",ylab="")
+	points(rep(2,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[2,]+1],yaxt='n')
+	points(rep(3,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[3,]+1],yaxt='n')
+	if (sum(x[4,]>0)) {
+		points(rep(4,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[4,]+1],yaxt='n')
+		axis(2,at=1:4,labels=c('user1','user2','user3','user4'))
+	} else {
+		axis(2,at=1:3,labels=c('user1','user2','user3'))
+	}
+}
--- events.R
+++ events.R
@@ -16,22 +16,23 @@ unique(events$type)
 
 # Summary of events by team 
 table(events$team)
+barplot(sort(table(events$team)),ylim=c(0,500))
 
 # Summary of events by team and user
-teams <- unique(events$team)
-n <- length(teams)
-close.screen(all.screens = TRUE)
-split.screen(c(3,4))
-for (i in 1:n) {
-    screen(i)
-    pie(table(events[events$team==teams[i],]$actor))
-}
+# teams <- unique(events$team)
+# n <- length(teams)
+# close.screen(all.screens = TRUE)
+# split.screen(c(3,4))
+# for (i in 1:n) {
+#     screen(i)
+#     pie(table(events[events$team==teams[i],]$actor))
+# }
 
-close.screen(all.screens = TRUE)
+# close.screen(all.screens = TRUE)
 
 # Get numbers of events per week
-startTime <- strptime("2017-01-19 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
-endTime <- strptime("2017-04-22 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
+startTime <- strptime("2017-01-10 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
+endTime <- strptime("2017-05-05 00:00:00 GMT", format = "%Y-%m-%d %H:%M:%S GMT")
 timeList <- c(startTime)
 while(startTime<endTime) {
     startTime <- startTime + (7*24*60*60)
@@ -51,14 +52,33 @@ events$week[j] <- (function(x,y) {
     return
 }) (events$created_at[j],timeList) }
 
+events$week <- as.factor(events$week)
+events$actor <- as.factor(events$actor)
+
 # Plot graph of events based on week
+# for (team in unique(events$team)) {
+# 	team_data <- events[events$team==team,]
+# 	plot(table(team_data[team_data$actor=='user1',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="red")
+# 	lines(table(team_data[team_data$actor=='user2',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="green")
+# 	lines(table(team_data[team_data$actor=='user3',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="brown")
+# 	if (is.element('user4',unique(team_data$actor))==TRUE) {
+# 		lines(table(team_data[team_data$actor=='user4',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="black")
+# 	}
+# }
+
+par(las=1)
+colRamp <- colorRampPalette(c("white", "black"),interpolate = c("linear"))
 for (team in unique(events$team)) {
-	team_data <- events[events$team==team,]
-	plot(table(team_data[team_data$actor=='user1',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="red")
-	lines(table(team_data[team_data$actor=='user2',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="green")
-	lines(table(team_data[team_data$actor=='user3',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="brown")
-	if (is.element('user4',unique(team_data$actor))==TRUE) {
-		lines(table(team_data[team_data$actor=='user4',]$week),xlim=c(0,15),ylim=c(0,50),type="l",col="black")
-	}
+    team_data <- events[events$team==team,]
+    x <- table(team_data$actor,team_data$week)
+    plot(rep(1,length(timeList)),pch = 15,cex = 5,ylim=c(0,5),col=colRamp(max(x)+1)[x[1,]+1],yaxt='n',main=paste("Color scale of number of events per week : ",team),xlab="Week",ylab="")
+    points(rep(2,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[2,]+1],yaxt='n')
+    points(rep(3,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[3,]+1],yaxt='n')
+    if (sum(x[4,]>0)) {
+        points(rep(4,length(timeList)), pch = 15,cex = 5, col=colRamp(max(x)+1)[x[4,]+1],yaxt='n')
+        axis(2,at=1:4,labels=c('user1','user2','user3','user4'))
+    } else {
+        axis(2,at=1:3,labels=c('user1','user2','user3'))
+    }
 }
 
